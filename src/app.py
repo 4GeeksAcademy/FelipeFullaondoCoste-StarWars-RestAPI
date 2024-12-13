88b1e9a5f8ad2484379aac2b82102232abf81eae
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-12-13 20:05:51 +0000

    seguimiento de clase JWT

--- src/app.py
+++ src/app.py
@@ -9,10 +9,15 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from routes import user_bp
+from flask_jwt_extended import JWTManager, jwt_required
+from datetime import timedelta
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.register_blueprint(user_bp, url_prefix="/user1")
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -21,20 +26,31 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+flask = JWTManager(app)
+
+app.config["SECRET_KEY"] = "clave secreta"
+app.config["JWT_ACESS_TOKEN_EXPIRES"] = timedelta(seconds=5)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -44,6 +60,8 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
---
+++ src/routes/user/user_route.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request, jsonify
+from models import db, User
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, jwt_required
+
+
+user_bp = Blueprint('user1',__name__)
+bcrypt = Bcrypt()
+
+@user_bp.route('/create',methods=['POST'])
+def create_user():
+    user_data = request.get_json()
+    new_user = User(**user_data)
+    db.session.add(new_user)
+    new_user.password = bcrypt.generate_password_hash(new_user.password).decode('utf-8')
+    print(new_user.password)
+    db.session.commit()
+    print(new_user)
+    return "Usuario creado",200
+
+@user_bp.route('/login',methods=['POST'])
+def login():
+    user_data = request.get_json()
+    user = User.query.filter_by(email=user_data["email"]).first()
+    if user and bcrypt.check_password_hash(user.password, user_data["password"]):
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({"msg": "Login corrector","access_token":access_token})
+    else:
+        return jsonify({"msg": "Email no registrado o datos incorrectos"})
+
+@user_bp.route('/get',methods=['GET'])
+@jwt_required()
+def get_user_list():
+    user_list = User.query.all()
+    user_list = [user.serialize() for user in user_list]
+    print(user_list)
+    return jsonify({"user_list":user_list})
+
+@user_bp.route('/get/<int:id>',methods=['GET'])
+@jwt_required()
+def get_ser(id):
+    user = User.query.get(id)
+    print(user)
+    return jsonify({"user":user.serialize()})
