ba089c9b35adfe997907fed7a86ebc1b8ab6d62d
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-12-20 01:37:19 +0000

    proyecto terminad

--- src/models/user/user_model.py
+++ src/models/user/user_model.py
@@ -2,7 +2,6 @@ from .. import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -10,7 +9,7 @@ class User(db.Model):
     favorites = db.relationship('Favorites', back_populates='user')
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.email}>'
 
     def serialize(self):
         return {
